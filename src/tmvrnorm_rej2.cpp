7b2990bbde38dcb261c651924e7124078ccd6cdb
ychwang0331 <93721946+ychwang0331@users.noreply.github.com>
2024-02-20 10:49:11 -0500

    Update tmvrnorm_rej2.cpp

--- src/tmvrnorm_rej2.cpp
+++ src/tmvrnorm_rej2.cpp
@@ -77,11 +77,12 @@ arma::mat trmvrnorm_rej_cpp(int n, arma::vec mu,arma::mat sigma, arma::vec lower
   int ncols = sigma.n_cols;
   arma::mat Y(ncols, n);
   arma::mat cholSigma(ncols,ncols);
+  arma::mat newMatrix = sigma.t() * sigma;
   int samplesRemaining = n;
   int totalAccepted = 0;
   Environment mvtnorm =  Environment::namespace_env("mvtnorm");
   Function mypmvn =  mvtnorm["pmvnorm"];
-  double alpha = as<double>(mypmvn(NumericVector(lower.begin(),lower.end()),NumericVector(upper.begin(),upper.end()),NumericVector(mu.begin(),mu.end()),R_NilValue,sigma));
+  double alpha = as<double>(mypmvn(NumericVector(lower.begin(),lower.end()),NumericVector(upper.begin(),upper.end()),NumericVector(mu.begin(),mu.end()),R_NilValue,newMatrix));
   cholSigma = sigma;
   if(verb>=3){
     Rcout << "Acceptance rate: " << alpha << "\n";
