c202041f4d5b979a772b27494baf0946d2e1eafb
ychwang0331 <93721946+ychwang0331@users.noreply.github.com>
2024-02-20 11:20:48 -0500

    Update tmvrnorm_rej2.cpp

--- src/tmvrnorm_rej2.cpp
+++ src/tmvrnorm_rej2.cpp
@@ -80,7 +80,7 @@ arma::mat trmvrnorm_rej_cpp(int n, arma::vec mu,arma::mat sigma, arma::vec lower
   arma::mat newMatrix(ncols,ncols);
   int samplesRemaining = n;
   int totalAccepted = 0;
-  arma::mat sigmat = sigma.t()
+  arma::mat sigmat = sigma.t();
   newMatrix = sigmat * sigma;
   cholSigma = sigma;
   Environment mvtnorm =  Environment::namespace_env("mvtnorm");
